ea2c39daed013015249ca57d11d896a6af26dd65
Evandro Couto <ecouto@icasei.com.br>
2017-12-01 14:35:36 -0200

    ajustes Readme

--- packer/default.sh
+++ packer/default.sh
@@ -22,7 +22,7 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install -y docker-ce
 
- install compose
+# install compose
  sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
 # install kubernetes
